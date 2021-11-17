891f68ff022bceaf2b29ca198bd1966485e6a6d9
ammkrn <ammkrn@tuta.io>
2021-11-17 16:17:44 -0600

    Using semiring

--- Mathlib/Data/UInt.lean
+++ Mathlib/Data/UInt.lean
@@ -2,6 +2,7 @@ import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Fin.Basic
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.GroupWithZero.Defs
+import Mathlib.Algebra.Ring.Basic
 
 lemma UInt8.size_positive : 0 < UInt8.size := by decide
 
@@ -13,178 +14,145 @@ lemma UInt64.size_positive : 0 < UInt64.size := by decide
 
 lemma USize.size_positive : 0 < USize.size := usize_size_gt_zero
 
-set_option hygiene false
-local macro "genIntDeclars" typeName:ident : command => do
-  let deltaName := Lean.mkIdent $ Lean.Name.mkSimple s!"instOfNat{typeName.getId}"
-  `(
-    namespace $typeName
-    instance : Inhabited (Fin size) where
-      default := Fin.ofNat' 0 size_positive
+lemma UInt8.val_eq_of_lt : a < UInt8.size → (UInt8.ofNat a).val = a := Fin.val_eq_of_lt
 
-    lemma val_eq_of_lt : a < size → (ofNat a).val = a := Fin.val_eq_of_lt
+lemma UInt32.val_eq_of_lt : a < UInt32.size → (UInt32.ofNat a).val = a := Fin.val_eq_of_lt
 
-    lemma sub_def (a b : $typeName) : a - b = ⟨a.val - b.val⟩ := rfl
+namespace UInt8
 
-    lemma mul_def (a b : $typeName) : a * b = ⟨a.val * b.val⟩ := rfl
+instance : Inhabited (Fin size) where
+  default := Fin.ofNat' 0 size_positive
 
-    lemma mod_def (a b : $typeName) : a % b = ⟨a.val % b.val⟩ := rfl
+instance : AddSemigroup UInt8 where
+  add_assoc := fun _ _ _ => congrArg mk (AddSemigroup.add_assoc _ _ _)
 
-    lemma add_def (a b : $typeName) : a + b = ⟨a.val + b.val⟩ := rfl
+instance : AddCommSemigroup UInt8 where
+  add_comm := fun _ _ => congrArg mk (AddCommSemigroup.add_comm _ _)
 
-    lemma eq_of_val_eq : ∀ {a b : $typeName}, a.val = b.val -> a = b
-    | ⟨f1⟩, ⟨f2⟩, h => congrArg mk h
+instance : Semigroup UInt8 where
+  mul_assoc := fun _ _ _ => congrArg mk (Semigroup.mul_assoc _ _ _)
 
-    lemma val_eq_of_eq : ∀ {a b : $typeName}, a = b -> a.val = b.val
-    | ⟨f1⟩, ⟨f2⟩, h => congrArg val h
+instance : Neg UInt8 where
+  neg a := mk (-a.val)
 
-    @[simp] lemma mk_val_eq : ∀ (a : $typeName), mk a.val = a
-    | ⟨a, _⟩ => rfl
+lemma sub_def (a b : UInt8) : a - b = ⟨a.val - b.val⟩ := rfl
 
-    instance : AddSemigroup $typeName where
-      add_assoc := fun _ _ _ => congrArg mk (AddSemigroup.add_assoc _ _ _)
+lemma mul_def (a b : UInt8) : a * b = ⟨a.val * b.val⟩ := rfl
 
-    instance : AddCommSemigroup $typeName where
-      add_comm := fun _ _ => congrArg mk (AddCommSemigroup.add_comm _ _)
+lemma mod_def (a b : UInt8) : a % b = ⟨a.val % b.val⟩ := rfl
 
-    instance : Semigroup $typeName where
-      mul_assoc := fun _ _ _ => congrArg mk (Semigroup.mul_assoc _ _ _)
+lemma add_def (a b : UInt8) : a + b = ⟨a.val + b.val⟩ := rfl
 
-    instance : CommSemigroup $typeName where
-      mul_comm := fun _ _ => congrArg mk (CommSemigroup.mul_comm _ _)
+lemma eq_of_val_eq : ∀ {a b : UInt8}, a.val = b.val -> a = b
+| ⟨f1⟩, ⟨f2⟩, h => congrArg mk h
 
-    instance : AddMonoid $typeName :=
-      let add_zero_ : ∀ (a : $typeName), a + 0 = a := fun a => by
-        have h0 : a.val + (0 : $typeName).val = a.val := AddMonoid.add_zero a.val
-        simp only [add_def, h0, mk_val_eq]
-    {
-      add_zero := add_zero_
-      zero_add := fun _ => by rw [add_comm]; exact add_zero_ _
-      nsmul := fun x a => ⟨AddMonoid.nsmul x a.val⟩
-      nsmul_zero' := fun x => congrArg mk (AddMonoid.nsmul_zero' x.val)
-      nsmul_succ' := fun n x => congrArg mk (AddMonoid.nsmul_succ' n x.val)
-    }
+lemma val_eq_of_eq : ∀ {a b : UInt8}, a = b -> a.val = b.val
+| ⟨f1⟩, ⟨f2⟩, h => congrArg val h
 
-    instance : AddCommMonoid $typeName where
-      add_comm := add_comm
+@[simp] lemma mk_val_eq : ∀ (a : UInt8), mk a.val = a
+| ⟨a, _⟩ => rfl
 
-    instance : Neg $typeName where
-      neg a := mk (-a.val)
-
-    instance : SubNegMonoid $typeName where
-      sub_eq_add_neg := fun _ _ => congrArg mk (SubNegMonoid.sub_eq_add_neg _ _)
-      gsmul := fun x a => ⟨SubNegMonoid.gsmul x a.val⟩
-      gsmul_zero' := fun a => congrArg mk (SubNegMonoid.gsmul_zero' a.val)
-      gsmul_succ' := fun m a => congrArg mk (SubNegMonoid.gsmul_succ' m a.val)
-      gsmul_neg' := fun m a => congrArg mk (SubNegMonoid.gsmul_neg' m a.val)
-
-    instance : Monoid $typeName :=
-      let mul_one_ : ∀ (a : $typeName), a * (1 : $typeName) = a := fun a => by
-        have h0 : a.val * (1 : $typeName).val = a.val := Monoid.mul_one a.val
-        simp only [mul_def, h0, mk_val_eq]
-    {
-      mul_one := mul_one_
-      one_mul := by intro a; rw [mul_comm]; exact mul_one_ a
-      npow_zero' := fun _ => rfl
-      npow_succ' := fun _ _ => rfl
-    }
-
-    instance : CommMonoid $typeName where
-      mul_comm := mul_comm
-
-      def addOverflows? (a b : $typeName) : Bool := size <= a.toNat + b.toNat
-
-      def mulOverflows? (a b : $typeName) : Bool := size <= a.toNat * b.toNat
-
-      def subUnderflows? (a b : $typeName) : Bool := a.toNat < b.toNat
-
-      def overflowingAdd (a b : $typeName) : (Bool × $typeName) := (size <= a.toNat + b.toNat, a + b)
-
-      def overflowingMul (a b : $typeName) : (Bool × $typeName) := (size <= a.toNat * b.toNat, a * b)
-
-      def underflowingSub (a b : $typeName) : (Bool × $typeName) := (a.toNat < b.toNat, a - b)
-
-      def checkedAdd (a b : $typeName) : Option $typeName :=
-        match a.overflowingAdd b with
-        | (true, _) => none
-        | (false, sum) => some (sum)
-
-      def checkedMul (a b : $typeName) : Option $typeName :=
-        match a.overflowingMul b with
-        | (true, _) => none
-        | (false, prod) => some (prod)
-
-      def checkedSub (a b : $typeName) : Option $typeName :=
-        match a.underflowingSub b with
-        | (true, _) => none
-        | (false, diff) => some (diff)
-
-      lemma checked_add_spec (a b : $typeName) : (a.checkedAdd b).isSome = true <-> a.toNat + b.toNat < size := by
-        by_cases size <= a.toNat + b.toNat <;>
-          simp_all [checkedAdd, Option.isSome, overflowingAdd, decide_eq_true, decide_eq_false]
-
-      lemma checked_mul_spec (a b : $typeName) : (a.checkedMul b).isSome = true <-> a.toNat * b.toNat < size := by
-        simp only [checkedMul, overflowingMul, Option.isSome]
-        refine Iff.intro ?mp ?mpr <;> intro h
-        case mp =>
-          by_cases hx : size <= a.toNat * b.toNat
-          case pos => simp only [(decide_eq_true_iff (size <= a.toNat * b.toNat)).mpr hx] at h
-          case neg => exact Nat.lt_of_not_le hx
-        case mpr => simp only [decide_eq_false (Nat.not_le_of_lt h : ¬size <= a.toNat * b.toNat)]
-
-      lemma checked_sub_spec (a b : $typeName) : (a.checkedSub b).isSome = true <-> b.toNat <= a.toNat := by
-        simp only [checkedSub, underflowingSub, Option.isSome]
-        refine Iff.intro ?mp ?mpr <;> intro h
-        case mp =>
-          by_cases hx : a.val < b.val
-          case pos => simp only [(decide_eq_true_iff (a.toNat < b.toNat)).mpr hx] at h
-          case neg => exact Nat.le_of_not_lt hx
-        case mpr => simp only [decide_eq_false (Nat.not_lt_of_le h : ¬a.toNat < b.toNat)]
-
-      lemma mod_lt (x : $typeName) {y : $typeName} : 0 < y -> x % y < y :=
-        fun y_pos => Fin.mod_lt x.val y_pos
-
-      lemma mod_eq_of_lt {a b : $typeName} : a < b -> a % b = a :=
-        fun a_lt_b => eq_of_val_eq (Fin.mod_eq_of_lt a_lt_b)
-
-    end $typeName
-  )
-
-genIntDeclars UInt8
-genIntDeclars UInt16
-genIntDeclars UInt32
-genIntDeclars UInt64
-genIntDeclars USize
-
-/- This one works, but only when `h0` is extracted -/
-lemma UInt8.zero_mul (a : UInt8) : 0 * a = 0 := by
-    simp only [mul_def]
-    have h0 : (0 : UInt8).val * a.val = (0 : Fin UInt8.size) := MonoidWithZero.zero_mul a.val
-    simp only [h0]
-
-/- Doesn't work; if you make the instances from `ofNat_class` `(priority := high)`, then
-the instances of `OfNat` do change, but simp still doesn't work -/
-lemma USize.zero_mul (a : USize) : 0 * a = 0 := by
-    -- For reference, `(0 : UInt).val = (0 : Fin USize.size) := rfl` works
-    simp only [mul_def]
-    have h0 : (0 : USize).val * a.val = (0 : Fin USize.size) := MonoidWithZero.zero_mul a.val
-    simp only [h0]
-    have h_by_rfl : (0 : USize).val * a.val = (0 : Fin USize.size) * a.val := rfl
-    simp only [h_by_rfl] at h0
-    simp only [h0]
-    simp only [MonoidWithZero.zero_mul a.val]
+instance : Semiring UInt8 where
+  add_zero := by
+    intro a
+    simp only [add_def]
+    simp only [Semiring.add_zero]
+    simp only [(Semiring.add_zero a.val : a.val + (0 : Fin UInt8.size) = a.val)]
+    simp only [(Semiring.add_zero a.val : a.val + (0 : UInt8).val = a.val)]
+    trace_state
     sorry
-
-/- Doesn't work by inlining -/
-lemma UInt8.zero_mul' (a : UInt8) : 0 * a = 0 := by
+  mul_one := by
+    intro a
     simp only [mul_def]
-    simp only [MonoidWithZero.zero_mul]
+    simp only [Semiring.mul_one]
+    simp only [(Semiring.mul_one a.val : a.val * (1 : Fin UInt8.size) = a.val)]
+    simp only [(Semiring.mul_one a.val : a.val * (1 : UInt8).val = a.val)]
+    trace_state
     sorry
+  one_mul := sorry
+  ofNat := sorry
+  zero_add := sorry
+  nsmul := sorry
+  nsmul_zero' := sorry
+  nsmul_succ' := sorry
 
-/- Doesn't work by inlining with a type annotation -/
-lemma UInt8.zero_mul'' (a : UInt8) : 0 * a = 0 := by
-    simp only [mul_def, (MonoidWithZero.zero_mul a.val : (0 : UInt8).val * a.val = (0 : Fin UInt8.size))]
+  zero_mul := sorry
+  mul_zero := sorry
+  npow := sorry
+  npow_zero' := sorry
+  npow_succ' := sorry
+  mul_add := sorry
+  add_mul := sorry
+  ofNat_succ := sorry
+
+end UInt8
+
+namespace USize
+instance : Inhabited (Fin size) where
+  default := Fin.ofNat' 0 size_positive
+
+instance : AddSemigroup USize where
+  add_assoc := fun _ _ _ => congrArg mk (AddSemigroup.add_assoc _ _ _)
+
+instance : AddCommSemigroup USize where
+  add_comm := fun _ _ => congrArg mk (AddCommSemigroup.add_comm _ _)
+
+instance : Semigroup USize where
+  mul_assoc := fun _ _ _ => congrArg mk (Semigroup.mul_assoc _ _ _)
+
+instance : Neg USize where
+  neg a := mk (-a.val)
+
+lemma add_def (a b : USize) : a + b = ⟨a.val + b.val⟩ := rfl
+
+lemma sub_def (a b : USize) : a - b = ⟨a.val - b.val⟩ := rfl
+
+lemma mul_def (a b : USize) : a * b = ⟨a.val * b.val⟩ := rfl
+
+lemma mod_def (a b : USize) : a % b = ⟨a.val % b.val⟩ := rfl
+
+lemma eq_of_val_eq : ∀ {a b : USize}, a.val = b.val -> a = b
+| ⟨f1⟩, ⟨f2⟩, h => congrArg mk h
+
+lemma val_eq_of_eq : ∀ {a b : USize}, a = b -> a.val = b.val
+| ⟨f1⟩, ⟨f2⟩, h => congrArg val h
+
+@[simp] lemma mk_val_eq : ∀ (a : USize), mk a.val = a
+| ⟨a, _⟩ => rfl
+
+instance : Semiring USize where
+  add_zero := by
+    intro a
+    simp only [add_def]
+    simp only [Semiring.add_zero]
+    simp only [(Semiring.add_zero a.val : a.val + (0 : Fin USize.size) = a.val)]
+    simp only [(Semiring.add_zero a.val : a.val + (0 : USize).val = a.val)]
+    trace_state
     sorry
+  mul_one := by
+    intro a
+    simp only [mul_def]
+    simp only [Semiring.mul_one]
+    simp only [(Semiring.mul_one a.val : a.val * (1 : Fin USize.size) = a.val)]
+    simp only [(Semiring.mul_one a.val : a.val * (1 : USize).val = a.val)]
+    trace_state
+    sorry
+  one_mul := sorry
+  ofNat := sorry
+  zero_add := sorry
+  nsmul := sorry
+  nsmul_zero' := sorry
+  nsmul_succ' := sorry
+  zero_mul := sorry
+  mul_zero := sorry
+  npow := sorry
+  npow_zero' := sorry
+  npow_succ' := sorry
+  mul_add := sorry
+  add_mul := sorry
+  ofNat_succ := sorry
 
+end USize
 
 namespace UInt8
 
